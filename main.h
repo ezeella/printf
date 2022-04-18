29ddf378bf1847b1502da26ba9df9c2524c23fc3
ezeella <myrealface1@gmail.com>
2022-04-18 13:44:11 -0700

    first commit

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
